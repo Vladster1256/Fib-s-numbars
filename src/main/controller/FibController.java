476f22e699f181e5b100e7becc9ca4b73a22c0cc
Vladster1256 <vcmariorocks@gmail.com>
2015-03-18 12:40:46 -0600

    Finished

--- src/main/controller/FibController.java
+++ src/main/controller/FibController.java
@@ -1,6 +1,47 @@
 package main.controller;
 
+import java.util.Scanner;
+
+
+
 public class FibController
 {
+	public Scanner $console;
+	
+	
+	public FibController()
+	{
+		$console = new Scanner(System.in);
+	}
+	
+	public void start()
+	{
+		
+		System.out.println("Please enter the int of what position you would like to calculate in the Fibbonacci sequence");
+		double toWhatPosition = $console.nextDouble();
+		calculateFib(toWhatPosition);
+	}
+	
 
+	
+	public void calculateFib(double upToNumber)
+	{
+		long startTime =System.currentTimeMillis();
+		double firstNumber = 0;
+		double secondNumber = 1;
+		double temp = 0;
+		double maxPosition = upToNumber;
+		System.out.println("Now calculating to position: " + upToNumber + " in the Fibonacci sequence.");
+		for(double position = 0; position < maxPosition; position++)
+		{
+			temp = firstNumber + secondNumber;
+			firstNumber = secondNumber;
+			secondNumber = temp;
+			System.out.println(firstNumber);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("The time it took to perform this opperation was " + (endTime - startTime) + " milliseconds");
+		System.out.println();
+		start();
+	}
 }
--- src/main/controller/FibRunner.java
+++ src/main/controller/FibRunner.java
@@ -2,5 +2,9 @@ package main.controller;
 
 public class FibRunner
 {
-
+	public static void main(String[]args)
+	{
+		FibController controller = new FibController();
+		controller.start();
+	}
 }
